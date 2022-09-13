62b0082b53baa5de904d905773088275ba9ccd2d
izabelakatarzyna <kolodziejczyk.swift@gmail.com>
2022-09-13 16:46:36 +0200

    js

---
+++ js1/script.js
@@ -0,0 +1,23 @@
+console.log("ELO");
+const x = 1;
+let y = 1;
+
+//x = 2;
+y = 3;
+console.log("y:", y, ", x =", x);
+
+const personName = "John";
+console.log(personName);
+
+//obiekty
+const person = {
+  name: "John",
+  age: 38,
+  autfit: {
+    hat: "Chanel",
+    shirt: "Nike",
+    boots: "Nike",
+  },
+};
+
+console.log(obj);

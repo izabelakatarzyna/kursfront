255bf366d483aa93e671d1dbfa8fb0f14dbb3fac
izabelakatarzyna <kolodziejczyk.swift@gmail.com>
2022-09-13 18:18:50 +0200

    js1

--- js1/script.js
+++ js1/script.js
@@ -5,19 +5,73 @@ let y = 1;
 //x = 2;
 y = 3;
 console.log("y:", y, ", x =", x);
-
-const personName = "John";
-console.log(personName);
-
-//obiekty
+console.personName = "John";
 const person = {
   name: "John",
   age: 38,
-  autfit: {
+  outfit: {
     hat: "Chanel",
     shirt: "Nike",
     boots: "Nike",
   },
 };
 
-console.log(obj);
+//console.log(person.outfit.boots);
+
+const people = ["John", "Joanna", "Josh"];
+
+console.log(people);
+
+function przyklad(argument = "default", drugiArgument) {
+  console.log("siemka");
+  console.log("wykonuje sie z: ", argument);
+  console.log("wykonuje sie z: ", drugiArgument);
+  console.log("nara");
+  return "zwracam wafrtość z " + argument;
+}
+
+//przyklad("argumentem");
+//przyklad();
+
+//const zwrot = przyklad("arg");
+
+//console.log( zwrot );
+
+console.log(console);
+
+const dog = {
+  voice: () => {
+    console.log("hau hau");
+  },
+};
+
+//dog.voice()
+
+//console.log(dokument);
+
+const h1 = document.querySelector("h1");
+console.log(h1);
+
+h1.innerHTML = "TEST";
+
+let count = 0;
+function counter() {
+  count++;
+  console.log(count);
+}
+
+//prxzypisanie wydarzenia do kliknięcia na h1
+h1.onclick = function () {
+  counter();
+};
+
+//warunek
+
+let a = 2;
+const b = 4;
+
+if (a < 2) {
+  console.log("a jest mniejsze od 2");
+} else {
+  console.log("a nie jest mniejsze od 2");
+}

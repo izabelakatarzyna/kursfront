b162e34790e0ac29c72aa663af18b60e2b003205
izabelakatarzyna <kolodziejczyk.swift@gmail.com>
2022-09-15 18:00:35 +0200

    maze8

--- js1/gra/script.js
+++ js1/gra/script.js
@@ -37,16 +37,16 @@ function makeWall(x, y, w, h, type = "wall") {
   //Tworzymy nowy element html
   const wall = document.createElement("div");
   //do stworzonego elementu dodajemy style
-  wall.style.cssText = `background: ${color};
-    width: ${w}%;
+  wall.style.cssText = `background:${color};
+    width:${w}%;
     height:${h}%;
     left:${x}%;
-    top: ${y}%;
-    position: absolute;
+    top:${y}%;
+    position:absolute;
     `;
 
   //dodajemy klasy do każdego diva
-  //wall.className = type
+  wall.className = "wall";
 
   if (type != "wall") {
     //jeżeli ściana nie jest zwykłym wallem dodaj jej typ jako klasę po spacji
@@ -66,7 +66,7 @@ const map = [
   [30, 40, 20, 10],
   [40, 50, 20, 10],
   [50, 60, 20, 10],
-  [60, 70, 20, 10],
+  [60, 70, 30, 10],
   [80, 80, 20, 20, "meta"],
 ];
 
@@ -83,6 +83,7 @@ const game = {
     meta: document.querySelector(".meta"),
     walls: document.querySelectorAll(".wall"),
   },
+  //metoda przygotowująca grę
   init() {
     game.buttons.start.onclick = function () {
       game.start();
@@ -90,74 +91,92 @@ const game = {
   },
   start() {
     game.buttons.start.onclick = "";
-    game.buttons.meta.addEventListener("mousemove", game.metaTrigger);
+    game.buttons.meta.addEventListener("mousemove", game.over);
+    //jeżeli nakierujesz myszkę na gamePlane po starcie to przegrywasz grę
     gamePlane.addEventListener("mousemove", game.gamePlaneListener);
+    //wyciągamy jako wall każdą ścianę osobno
     for (const wall of game.buttons.walls) {
+      //Jeżeli  Twój kurs jest na klasie wall to nie wyzwalaj żadnych innych słuchaczy
       wall.addEventListener("mousemove", game.wallListener);
     }
     console.log("GAME STARTED");
   },
 
-  wallListener() {
+  wallListener(e) {
     e.stopPropagation();
   },
   gamePlaneListener(e) {
     game.over(false);
   },
-  metaTrigger() {
-    game.over(true);
-  },
+  //   metaTrigger(result) {
+  //     if(result{
+  //         console.log("YOU WIN");
+  //     })
   over(result) {
     if (result) {
-      console.log("YOU WIN");
+      modal.show("wygrana");
     } else {
-      console.log("YOU LOSE");
+      modal.show("przegrana");
     }
-    game.buttons.meta.removeEventListener("mousemove", game.metaTrigger);
+    game.buttons.meta.removeEventListener("mousemove", game.over);
 
-    gamePlane.removeEventListener("mousemove", game.checkIfOnBoard);
+    gamePlane.removeEventListener("mousemove", game.gamePlaneListener);
     for (const wall of game.buttons.walls) {
       wall.removeEventListener("mousemove", game.wallListener);
     }
     game.init();
   },
 };
-game.init();
-
 //komunikaty
 
 const modal = {
   dom: document.createElement("div"),
+  h1: document.createElement("h1"),
   init() {
-    modal.dom.style.sccText = `
-        border: 10px dashed red;
+    modal.dom.style.cssText = `
+        border: 10px dashed green;
         position: fixed;
         width: 80vw;
         height: 80vh;
-        left: 1-vw;
-        top: 10vw;
-        background: red;
+        left: 10vw;
+        top: 10vh;
+        background: #aa6969;
         display: flex;
         flex-direction: column;
-        align-items: center:
+        align-items: center;
         justify-content: center;
+        text-align: center;
+        border-radious: 10px;
         `;
 
     document.body.append(modal.dom);
 
-    const h1 = document.createElement("h1");
-    modal1.dom.append(h1);
+    modal.h1.innerHTML = "h1";
+    modal.dom.append(modal.h1);
 
     const button = document.createElement("button");
     button.innerHTML = "OK";
-    modal1.dom.append(button);
+    button.style.cssText = `
+    padding:1rem 4rem
+    border-radious:1rem;
+    cursor:pointer;
+    `;
+    button.onclick = function () {
+      modal.hide();
+    };
+    modal.dom.append(button);
   },
-  show() {
+  show(text) {
     modal.dom.style.display = "flex";
+    modal.h1.innerHTML = text;
   },
 
   hide() {
     modal.dom.style.display = "none";
   },
 };
-modal.show();
+modal.init();
+modal.show(
+  "Kliknij na niebieski kafelek aby rozpocząć grę<br/>Przesuń kursor na pomarańczowy kafelek aby wygrać"
+);
+game.init();
